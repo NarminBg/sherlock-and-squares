84627d630e2377f5cea2d5f7a0c5d7ec85b397ff
[Narmin Bagirova] <[narminnbagirova@gmail.com]>
2022-12-29 16:43:54 +0400

    Add project files.

---
+++ sherlock and squares/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace sherlock_and_squares
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int a=Convert.ToInt32(Console.ReadLine());
+            int b=Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+            for (int i = Math.Min(a, b); i <= Math.Max(a, b); i++)
+            {
+                if((int)Math.Sqrt(i)==Math.Sqrt(i));
+                {
+                    count++;
+                }
+                
+            }
+            Console.WriteLine(count);
+
+
+
+
+        }
+
+    }
+}
